fce1bb70e83ad9b4702769836c80dc1cf0fd32d9
Jacob Studstill <jacob.studstill@gmail.com>
2023-01-05 05:13:08 -0500

    Added questions to the javascript page containing questions

--- questions.js
+++ questions.js
@@ -1,5 +1,45 @@
 var questions = [
     {
+        title: "Arrays in Javascript can be used to store ______",
+        choices: ["numbers and strings" , "other arrays" , "booleans", "All of the above"],
+        answer: "All of the above"
+    },
+    {
+        title: "Commonly used data types DO NOT include: ",
+        choices: ["strings" , "booleans" , "alerts", "numbers"],
+        answer: "alerts"
+    },
+    {
+        title: "The condition in an if / else statement is enclosed within ______.",
+        choices: ["quotes" , "square brackets" , "curly brackets", "parentheses"],
+        answer: "parentheses"
+    },
+    {
+        title: "What is a question",
+        choices: ["yes" , "no" , "maybe", "nope"],
+        answer: "yes"
+    },
+    {
+        title: "What is a question",
+        choices: ["yes" , "no" , "maybe", "nope"],
+        answer: "yes"
+    },
+    {
+        title: "What is a question",
+        choices: ["yes" , "no" , "maybe", "nope"],
+        answer: "yes"
+    },
+    {
+        title: "What is a question",
+        choices: ["yes" , "no" , "maybe", "nope"],
+        answer: "yes"
+    },
+    {
+        title: "What is a question",
+        choices: ["yes" , "no" , "maybe", "nope"],
+        answer: "yes"
+    },
+    {
         title: "What is a question",
         choices: ["yes" , "no" , "maybe", "nope"],
         answer: "yes"
